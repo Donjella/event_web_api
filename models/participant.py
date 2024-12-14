6cae34e9ae1025269ba2a03b0d6b8e82b09e4ad6
Donjella <brendonang@gmail.com>
2024-12-15 00:22:18 +1100

    Create participant model and schema

---
+++ models/participant.py
@@ -0,0 +1,22 @@
+from init import db, ma
+from marshmallow import fields
+
+class Participant(db.Model):
+    __tablename__ = "participants"
+    participant_id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+
+    # Relationships
+    event_participants = db.relationship("EventParticipant", back_populates="participant", cascade="all, delete")
+
+class ParticipantSchema(ma.Schema):
+    event_participants = fields.List(fields.Nested("EventParticipantSchema"), exclude=["participant"])
+
+    class Meta:
+        fields = ("participant_id", "first_name", "last_name", "email", "phone", "event_participants")
+
+participant_schema = ParticipantSchema()
+participants_schema = ParticipantSchema(many=True)

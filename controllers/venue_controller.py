dc2bd016cc8c830395fd567947a1909b982d611e
Donjella <brendonang@gmail.com>
2024-12-16 12:48:14 +1100

    Implement create operation on Venue model

--- controllers/venue_controller.py
+++ controllers/venue_controller.py
@@ -14,3 +14,24 @@ def get_venues():
     venues_list = db.session.scalars(stmt)
     return venues_schema.dump(venues_list)
 
+# Create - /venues - POST
+@venues_bp.route("/", methods=["POST"])
+def create_venue():
+    try:
+        body_data = venue_schema.load(request.get_json())
+        new_venue = Venue(
+            name=body_data.get("name"),
+            street_address=body_data.get("street_address"),
+            city=body_data.get("city"),
+            state=body_data.get("state"),
+            postcode=body_data.get("postcode"),
+            capacity=body_data.get("capacity")
+        )
+        db.session.add(new_venue)
+        db.session.commit()
+        return venue_schema.dump(new_venue), 201
+    except IntegrityError as err:
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 400
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+            return {"message": "Venue name must be unique"}, 400

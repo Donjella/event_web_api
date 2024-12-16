274f7902a094a612a889ac7347467d03de6d0a09
Donjella <brendonang@gmail.com>
2024-12-16 18:34:08 +1100

    Implement update operation to Event model

--- controllers/event_controller.py
+++ controllers/event_controller.py
@@ -12,8 +12,8 @@ events_bp = Blueprint("events", __name__, url_prefix="/events")
 # Create - /events - POST
 @events_bp.route("/", methods=["POST"])
 def create_event():
-    if not request.json:
-        return {"message": "Request body must be JSON"}, 400
+    if not request.data or request.data.strip() == b"":  # Handle missing JSON body
+        return {"message": "Request body must be JSON and cannot be empty."}, 400
 
     try:
         body_data = event_schema.load(request.get_json())
@@ -49,5 +49,37 @@ def get_event(event_id):
     event = db.session.scalar(stmt)
     if event:
         return event_schema.dump(event), 200
+    else:
+        return {"message": f"Event with id {event_id} not found"}, 404
+    
+# Update - /events/<event_id> - PUT/PATCH
+@events_bp.route("/<int:event_id>", methods=["PUT", "PATCH"])
+def update_event(event_id):
+    if not request.data or request.data.strip() == b"":  
+        return {"message": "Request body must be JSON and cannot be empty"}, 400
+
+    try:
+        body_data = request.get_json()  
+        if body_data is None:  
+            return {"message": "Malformed JSON: Request body must be valid JSON"}, 400
+
+        body_data = event_schema.load(body_data, partial=True)
+    except ValidationError as err:
+        return format_validation_error(err)
+
+    stmt = db.select(Event).filter_by(event_id=event_id)
+    event = db.session.scalar(stmt)
+    if event:
+        try:
+            event.name = body_data.get("name") or event.name
+            event.description = body_data.get("description") or event.description
+            event.date = body_data.get("date") or event.date
+            event.time = body_data.get("time") or event.time
+            event.organiser_id = body_data.get("organiser_id") or event.organiser_id
+            event.venue_id = body_data.get("venue_id") or event.venue_id
+            db.session.commit()
+            return event_schema.dump(event), 200
+        except IntegrityError as err:
+            return format_integrity_error(err)
     else:
         return {"message": f"Event with id {event_id} not found"}, 404
